a82e57ac4de0659263a6ad4c8881bae421d83150
Qiang Wang <qiang.wang@zuora.com>
2017-08-22 23:09:09 +0800

    Hello world in Go.

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+        "fmt"
+        "github.com/archerwq/go-lab/strutil"
+       )
+
+func main() {
+  fmt.Printf("Hello Go!\n")
+  fmt.Printf(strutil.Reverse("Hello Go!"))
+}
---
+++ strutil/reverse.go
@@ -0,0 +1,11 @@
+// Package strutil contains utility functions for working with string.
+package strutil
+
+// Reverse function returns its argument string reversed rune-wise left to right.
+func Reverse(str string) string {
+    r := []rune(str)
+    for i, j := 0, len(r) - 1; i <= len(r) / 2; i, j = i + 1, j - 1 {
+        r[i], r[j] = r[j], r[i]
+    }
+    return string(r)
+}
